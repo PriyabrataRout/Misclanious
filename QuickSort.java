55d4f43ea3d5a062841a871d3a60e8af0ea9e329
Priyabrata Rout <30842804+PriyabrataRout@users.noreply.github.com>
2017-09-26 02:10:13 +0530

    Add files via upload

---
+++ QuickSort.java
@@ -0,0 +1,16 @@
+package com.priya.Algos;
+
+public class QuickSort {
+	int[] inArray = { 5, 7, 8, 1, 3, 9, 2, 6, 4 };
+
+	public void partitionArray(int pivot) {
+		int leftPionter=-1;
+		int rightPointer=inArray.length;
+				
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
